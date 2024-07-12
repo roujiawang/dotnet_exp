137a266be6decc7144cd7c4c1f9a61195551a589
Roujia <rjwang@umich.edu>
2024-07-12 15:00:36 -0400

    created a simple console app

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace HelloWorld;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // adapted from https://learn.microsoft.com/en-us/dotnet/core/tutorials/with-visual-studio-code?pivots=dotnet-8-0
+        Console.WriteLine("What is your name?");
+        var name = Console.ReadLine();
+        name = string.IsNullOrWhiteSpace(name)? "Jack": name;  // default name
+        var currentDate = DateTime.Now;
+        Console.WriteLine($"{Environment.NewLine}Hello, {name}, now is {currentDate:d} at {currentDate:t}!");
+        Console.WriteLine($"Press any key to exit.");
+        Console.ReadKey(true);  // read pressed key info, but suppress any output
+    }
+}
