af873b16be6a4415923dc5f61d983ee4ccadd86b
Roujia <rjwang@umich.edu>
2024-07-12 16:27:57 -0400

    added a class library project

---
+++ ClassLibraryProjects/StringLibrary/Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace UtilityLibraries;
+
+public static class StringLibrary
+{
+    // extension method for the string class;
+    // checks if a string starts with an uppercase letter
+    public static bool StartsWithUpper(this string? str)
+    {
+        if (string.IsNullOrWhiteSpace(str))
+            return false;
+        
+        char ch = str[0];
+        return char.IsUpper(ch);
+    }
+}
