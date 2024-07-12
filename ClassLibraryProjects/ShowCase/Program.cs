cc3158272ced3bf6e4efef4f16a2a53c6eb79050
Roujia <rjwang@umich.edu>
2024-07-12 16:44:03 -0400

    added a console app to the solution, and added a project reference

---
+++ ClassLibraryProjects/ShowCase/Program.cs
@@ -0,0 +1,41 @@
+﻿using UtilityLibraries;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int row = 0;
+
+        do
+        {
+            if (row == 0 || row >= 25)
+                ResetConsole();  // call local method
+            
+            string? input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input))
+                break;
+            
+            Console.WriteLine($"Input: {input}");
+            Console.WriteLine("Begins with uppercase? " +
+                $"{(input.StartsWithUpper()? "Yes": "No")}.");
+            Console.WriteLine();
+            row += 4;
+        } while (true);
+
+        return;
+
+
+        // local method
+        void ResetConsole()
+        {
+            if (row > 0)
+            {
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+            Console.Clear();
+            Console.WriteLine($"Press <Enter> only to exit; otherwise, enter a string and press <Enter>:{Environment.NewLine}");
+            row = 3;
+        }
+    }
+}
